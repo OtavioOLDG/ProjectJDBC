5f6463343856f6b673ec4db63aa68fd50314fdd6
Otavio L. de Giacometti <otaviolg2000@gmail.com>
2024-01-15 21:41:53 -0300

    Creating department

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,40 +1,15 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-import db.DB;
-import db.DbIntegrityException;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Connection conn = null;
-		PreparedStatement pst = null;
+		Department obj = new Department(1, "Teste");
 		
-		try {
-			conn = DB.getConnection();
-			
-			pst = conn.prepareStatement(
-					"DELETE FROM Department "
-					+"Where "
-					+ "Id = ?");
-			
-			pst.setInt(1, 2);
-
-			
-			int rowsAffected = pst.executeUpdate();
-			
-			System.out.println("Rows affected = " + rowsAffected);
-		}
-		catch(SQLException e) {
-			throw new DbIntegrityException(e.getMessage());
-		}
-		finally {
-			DB.closePrepareStatement(pst);
-			DB.closeConnection();
-		}
+		System.out.println(obj);
+		
+		
 	}
 }
