9cb19f4980b881f2880480ced05876bb06488471
Otavio L. de Giacometti <otaviolg2000@gmail.com>
2024-01-19 16:54:32 -0300

    update Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,5 @@
 package application;
 
-import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -28,10 +27,16 @@ public class Program {
 		List<Seller> list2 = sellerDao.findAll();
 		list2.forEach(System.out::println);
 		
-		System.out.println("\n=== TEST 4: seller insert ===");
+		/*System.out.println("\n=== TEST 4: seller insert ===");
 		Seller seller2 = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
 		sellerDao.insert(seller2);
-		System.out.println("Inserted! New id: " + seller2.getId() + ";");
+		System.out.println("Inserted! New id: " + seller2.getId() + ";");*/
+		
+		System.out.println("\n=== TEST 4: seller update ===");
+		Seller seller2 = sellerDao.findById(10);
+		seller2.setName("Martha Waine");
+		sellerDao.update(seller2);
+		System.out.println("Update completed");
 		
 		
 	}
